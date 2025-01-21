6fbaaeb20ed550470bfaa8f659dceaf8a7951ae0
RenanAdao <renanadao@gmail.com>
2025-01-21 00:09:58 -0300

    Implementacao da funcao adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo(){
+    let amigo = document.getElementById('amigo').value;    
+    if (amigo == ''){
+        alert ('Por favor, insira um nome.');        
+    }else{
+        for (let i=0; i<amigos.length; i++){            
+            if (amigos[i].toLowerCase() == amigo.toLowerCase()){
+                alert ('Este amigo já foi adicionado!');
+                return;
+            }
+        }
+        amigos.push(amigo);
+        limparCampo();
+    }
+}
+
+function limparCampo(){
+    let amigo = document.getElementById('amigo');
+    amigo.value = '';
+}
